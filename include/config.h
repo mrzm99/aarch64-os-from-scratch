e78a4c080ef55cc2e9d81c3ceedb04ece0b37a32
mrzm99 <mrzm.cpp@gmail.com>
2026-05-04 23:02:18 +0900

    feat: enable mmu

---
+++ include/config.h
@@ -0,0 +1,15 @@
+/*------------------------------------------------------*/
+/*!
+ *      @file   config.h
+ *      @date   2026.xx.xx
+ *      @author mrzm99
+ *      @brief  kernel config
+ *      @note
+ */
+/*------------------------------------------------------*/
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define PAGE_SIZE                   (4096)          //!< Page Size
+
+#endif
